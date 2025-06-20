82f895692dd2bd2671da5a8fa99c4971ebc77b82
banelexhamlashe345 <banelebanele938@gmail.com>
2025-06-20 11:40:59 +0200

    Update app.py

--- app.py
+++ app.py
@@ -407,6 +407,6 @@ def get_history():
 if __name__ == "__main__":
     import os
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port, debug=os.environ.get('FLASK_DEBUG', 'false').lower() == 'true')
 
 
