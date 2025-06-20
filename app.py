f174af19713863c1ab7aa2594305024fc8862baa
banelexhamlashe345 <banelebanele938@gmail.com>
2025-06-20 12:03:18 +0200

    Update app.py

--- app.py
+++ app.py
@@ -406,6 +406,6 @@ def get_history():
 
 if __name__ == "__main__":
     # For local development
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=False))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=False)
 
 
